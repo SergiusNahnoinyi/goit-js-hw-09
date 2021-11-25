a0e84f89bdec27efe693d24d41252cf96f12b897
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-25 17:11:06 +0200

    Выполняет первую задачу

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+'use strict';
 
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.setAttribute('disabled', true);
+  stopButton.removeAttribute('disabled');
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(timerId);
+  startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', true);
+});
