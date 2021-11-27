17e95061f98be3497f909dc47b361f76449d7b5c
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-27 13:46:48 +0200

    Реализует выбор валидной даты(будущей)

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,13 +5,23 @@ import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
 
+const startButton = document.querySelector('button[data-start]');
+
+startButton.setAttribute('disabled', true);
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const currentTime = Date.now();
+    if (currentTime >= selectedDates[0]) {
+      alert('Please choose a date in the future');
+    } else {
+      startButton.removeAttribute('disabled');
+      console.log(selectedDates[0]);
+    }
   },
 };
 
