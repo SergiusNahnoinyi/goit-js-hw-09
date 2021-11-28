3c095d0fe3d3a7deafe9c68bcf0260ad736ad583
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-28 14:47:01 +0200

    Реализует функцию для вызова функции  createPromise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,23 @@
+'use strict';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget;
+  let delayValue = Number(delay.value);
+  let stepValue = Number(step.value);
+  let amountValue = Number(amount.value);
+
+  for (let position = 1; position <= amountValue; position += 1) {
+    createPromise(position, delayValue);
+    delayValue += stepValue;
+    console.log(position, delayValue);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
